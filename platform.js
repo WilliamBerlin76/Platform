120241d2836f85ffa1160aa5bb4f3c24b749c001
WilliamBerlin76 <william.berlin76@gmail.com>
2020-07-31 20:15:34 -0500

    now listening to keypresses

--- platform.js
+++ platform.js
@@ -1,17 +1,16 @@
 var context, controller, sprite, loop;
 
-context = document.querySelector('canvas');
+context = document.querySelector('canvas').getContext('2d');
 
-context.height = 300;
-context.width = 500;
+context.canvas.height = 700;
+context.canvas.width = 1000;
 
-context.style.background = 'grey';
 
 sprite = {
-    height: 32,
-    width: 32,
+    height: 50,
+    width: 50,
     jumping: false,
-    x: 250,
+    x: 500,
     y: 0,
     x_velocity: 0,
     y_velocity: 0
@@ -28,14 +27,41 @@ controller = {
         switch(e.keyCode){
 
             case 37:
-                controller.left = key_state
+                controller.left = keyState
+                console.log('l')
                 break;
             case 38:
-                controller.up = key_state
+                controller.up = keyState
+                console.log('u')
                 break;
             case 39:
-                controller.right = key_state
+                controller.right = keyState
+                console.log('r')
                 break;
         };
     }
 };
+
+/////////////////LOOP/////////////////////
+
+loop = () => {
+
+
+}
+
+context.fillStyle = "grey"
+context.fillRect(0,0, context.canvas.width, context.canvas.width);
+context.beginPath();
+context.fillStyle = "purple";// hex for red
+context.rect(sprite.x, sprite.y, sprite.width, sprite.height);
+context.fill();
+// context.strokeStyle = "#202830";
+// context.lineWidth = 4;
+// context.beginPath();
+// context.moveTo(0, 164);
+// context.lineTo(320, 164);
+// context.stroke();
+
+window.addEventListener("keydown", controller.keyListener)
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(loop);
